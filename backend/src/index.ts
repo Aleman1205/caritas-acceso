cdd169eaf58a11bc95952a7234b029d11be50771
sand1no1 <64820057+sand1no1@users.noreply.github.com>
2025-09-18 01:29:57 -0600

    chore: creacion de archivo donde estarán las llamadas a las rutas

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/sedes");
+
+app.listen(PORT, () => {
+  console.log(`Server running at PORT ${PORT}`);
+});
