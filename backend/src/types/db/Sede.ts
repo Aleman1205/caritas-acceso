131de6c672f1a654531ab3a670061dd02ded0161
sand1no1 <64820057+sand1no1@users.noreply.github.com>
2025-09-17 23:56:57 -0600

    fix: implementacion con nuevos atributos HoraInicio y HoraFinal

--- backend/src/types/db/Sede.ts
+++ backend/src/types/db/Sede.ts
@@ -1,5 +1,9 @@
-export default interface Sede {
-    Id: number
+import type { RowDataPacket } from "mysql2"
+
+export default interface Sede extends RowDataPacket {
+    Id?: number
     Ubicacion: string
+    HoraInicio: string
+    HoraFinal: string
     Descripcion?: string | null
 }
---
+++ backend/src/types/db/SedeServicio.ts
@@ -0,0 +1,9 @@
+export default interface SedeServicio {
+    IdSede: number
+    IdServicio: number
+    Descripcion: string
+    Capacidad: number
+    Precio: number
+    HoraInicio: string
+    HoraFinal: string
+}
