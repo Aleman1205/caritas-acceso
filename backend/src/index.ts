fb89110726447adcab9bc4771f2a924ba58561be
Ernesto Sandino Ortiz De León <64820057+sand1no1@users.noreply.github.com>
2025-09-19 21:25:55 -0600

    feat: nueva ruta en index para servicios

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import sedeRoutes from "./routes/sede.js"
+import serviciosRoutes from "./routes/servicio.js"
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/sedes", sedeRoutes);
+app.use("/api/servicios", servicioRoutes);
 
 const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
