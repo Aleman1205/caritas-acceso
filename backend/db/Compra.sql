fd0ebbc0f31c783c46b6fbbe4d244e94e26cd26a
sbrsbr772 <a01660035@tec.mx>
2025-09-23 01:05:58 -0600

    Create Compra.sql

---
+++ backend/db/Compra.sql
@@ -0,0 +1,35 @@
+DELIMITER $$
+
+CREATE PROCEDURE CompraDeServicio (
+    IN IdTransaccion VARCHAR(200),
+    IN Total DECIMAL(6,2),
+    IN Fecha DATETIME, 
+    IN IdSede INT, 
+    IN IdServicio INT
+)
+
+-- Cambiar esto
+BEGIN
+    -- Verificar que exista la sede
+    IF EXISTS (SELECT 1 FROM Sede WHERE Id = p_IdSede) THEN
+        -- Verificar que exista el servicio
+        IF EXISTS (SELECT 1 FROM Servicio WHERE Id = p_IdServicio) THEN
+            -- Verificar que no exista ya la relación
+            IF NOT EXISTS (
+                SELECT 1
+                FROM SedeServicio
+                WHERE IdSede = p_IdSede
+                  AND IdServicio = p_IdServicio
+            ) THEN
+                INSERT INTO SedeServicio (
+                    IdSede, IdServicio, Descripcion, Capacidad, Precio, HoraInicio, HoraFinal
+                )
+                VALUES (
+                    p_IdSede, p_IdServicio, p_Descripcion, p_Capacidad, p_Precio, p_HoraInicio, p_HoraFinal
+                );
+            END IF;
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
