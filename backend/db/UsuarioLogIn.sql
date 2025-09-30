82b2d5f442e8dbd0fd8d9a8656ed26f0b9eee8b4
sbrsbr772 <a01660035@tec.mx>
2025-09-30 14:46:48 -0600

    Update UsuarioLogIn.sql

--- backend/db/UsuarioLogIn.sql
+++ backend/db/UsuarioLogIn.sql
@@ -1,8 +1,13 @@
 DELIMITER $$
 -- Posible inicio de sesión por medio del uso de la tabla Usuario Sede.
-CREATE PROCEDURE InicioSesion (
-    IN Email VARCHAR(300),
-    IN IdSede INT
+CREATE PROCEDURE Usuario_InicioSesion (
+    IN email VARCHAR(300),
+    IN telefono VARCHAR(25),
+    IN nombre VARCHAR(70),
+    IN apellido VARCHAR(50),
+    IN fotoURL VARBINARY,
+    IN FechaNacimiento DATE,
+    IN ID_TIPOUSUARIO INT
 )
 
 -- EDITARR
