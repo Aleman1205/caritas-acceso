954459dc1fe3af8f18f4e1772cdd56bfe15ab1b1
sbrsbr772 <a01660035@tec.mx>
2025-09-30 14:14:41 -0600

    Update UsuarioLogIn.sql

--- backend/db/UsuarioLogIn.sql
+++ backend/db/UsuarioLogIn.sql
@@ -1,1 +1,34 @@
+DELIMITER $$
+
+CREATE PROCEDURE InicioSesion (
+    IN Email VARCHAR(300),
+    IN IdSede INT
+)
+
+-- EDITARR
+-- Creas un create procedure en el que se ejecute la compra del servicio. 
+BEGIN
+    -- Verificar que exista la sede
+    IF EXISTS (SELECT 1 FROM Sede WHERE Id = p_IdSede) THEN
+        -- Verificar que exista el servicio
+        IF EXISTS (SELECT 1 FROM Servicio WHERE Id = p_IdServicio) THEN
+            -- Verificar que no exista ya la relación
+            IF NOT EXISTS (
+                SELECT 1
+                FROM SedeServicio
+                WHERE IdSede = p_IdSede
+                  AND IdServicio = p_IdServicio
+            ) THEN
+                INSERT INTO SedeServicio (
+                    IdSede, IdServicio, Descripcion, Capacidad, Precio, HoraInicio, HoraFinal
+                )
+                VALUES (
+                    p_IdSede, p_IdServicio, p_Descripcion, p_Capacidad, p_Precio, p_HoraInicio, p_HoraFinal
+                );
+            END IF;
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
 
