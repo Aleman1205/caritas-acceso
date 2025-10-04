9d1e6bcf5ffcaac14c763c55b11ce3dd150c84e3
sbrsbr772 <a01660035@tec.mx>
2025-10-04 17:12:13 -0600

    Update ruta.ts (importaciones pendientes)

--- backend/src/routes/ruta.ts
+++ backend/src/routes/ruta.ts
@@ -1,1 +1,23 @@
+import { Router } from "express";
+import RutaDbService from "../db/ruta.js";
+import RutaController from "../controllers/ruta.js";
+import RutaHttpHandler from "../handlers/ruta.js";
+import { dbPool } from "../config/db/mysql.js";
+import RutaValidadorRequest from "../utils/validadores/requests/ruta.js";
 
+const router = Router();
+
+// wiring de dependencias
+const service = new RutaDbService(dbPool);
+const controller = new RutaController(service);
+const validadorRequest = new RutaValidadorRequest();
+const handler = new RutaHttpHandler(controller, validadorRequest);
+
+// rutas
+router.post("/crear", handler.create.bind(handler));
+router.get("/obtener", handler.getAll.bind(handler));
+router.get("/obtener/:Id", handler.getAll.bind(handler));
+router.put("/modificar/:Id", handler.update.bind(handler));
+router.delete("/eliminar", handler.deleteMany.bind(handler));
+
+export default router;
