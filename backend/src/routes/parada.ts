ae610e80d2ba04f47d3d285b2c4ff39285d801c5
sand1no1 <64820057+sand1no1@users.noreply.github.com>
2025-10-05 00:05:31 -0600

    feat: router con metodos http de parada

--- backend/src/routes/parada.ts
+++ backend/src/routes/parada.ts
@@ -1,1 +1,23 @@
+import { Router } from "express";
+import ParadaDbService from "../db/parada.js";
+import ParadaController from "../controllers/parada.js";
+import ParadaHttpHandler from "../handlers/parada.js";
+import { dbPool } from "../config/db/mysql.js";
+import ParadaValidador from "../utils/validadores/requests/parada.js";
 
+const router = Router();
+
+// wiring de dependencias
+const service = new ParadaDbService(dbPool);
+const controller = new ParadaController(service);
+const validadorRequest = new ParadaValidador();
+const handler = new ParadaHttpHandler(controller, validadorRequest);
+
+// rutas
+router.post("/crear", handler.create.bind(handler));
+router.get("/obtener", handler.getAll.bind(handler));
+router.get("/obtener/:Id", handler.getAll.bind(handler));
+router.put("/modificar/:Id", handler.update.bind(handler));
+router.delete("/eliminar", handler.deleteMany.bind(handler));
+
+export default router;
