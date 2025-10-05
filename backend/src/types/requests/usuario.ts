7fc257938d266dc51c93533096365c7524b5e99d
sbrsbr772 <a01660035@tec.mx>
2025-10-05 15:10:56 -0600

    Update usuario.ts

--- backend/src/types/requests/usuario.ts
+++ backend/src/types/requests/usuario.ts
@@ -1,1 +1,28 @@
+import type { ParamsDictionary } from "express-serve-static-core";
+import type { ParsedQs } from "qs";
+import type { Request } from "express";
 
+export interface Params extends ParamsDictionary {
+	Id?: string;
+}
+export interface Body {
+	Email?: string;
+	Telefono?: string;
+	Nombre?: string;
+	Apellido?: string;
+	FotoUrl?: string; 
+	FechaNacimiento?: string;
+	IdTipoUsuario?: int;
+	Ids?: string[]; // para soportar deleteUsuario
+}
+export interface Query extends ParsedQs {
+	Email?: string;
+  Telefono?: string;
+  Nombre?: string;
+	Apellido?: string;
+	FotoUrl?: string; 
+	FechaNacimiento?: string;
+	IdTipoUsuario?: string;
+}
+
+export type SedeRequest = Request<Params, {}, Body, Query>;
