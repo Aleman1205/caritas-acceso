cf15db17c40757fd1d52057b78a36e8a77bb4ce5
sbrsbr772 <a01660035@tec.mx>
2025-10-04 20:08:27 -0600

    Update AgregarUsuario.sql

--- backend/db/AgregarUsuario.sql
+++ backend/db/AgregarUsuario.sql
@@ -7,30 +7,25 @@ CREATE PROCEDURE AgregarUsuario (
     IN telefono VARCHAR(25),
     IN nombre VARCHAR(70),
     IN apellido VARCHAR(50),
-    IN fotoURL VARBINARY,
-    IN FechaNacimiento DATE,
-    IN ID_TIPOUSUARIO INT
+    IN fotourl LONGBLOB,
+    IN fechaNacimiento DATE,
+    IN idTipoUsuario INT,
 )
 
   -- Creas un create procedure en el que sea agregue un usuario y que también cheque si dicho usuario, ya existe. 
   
 BEGIN
-    -- Verificar que exista la sede
-    IF EXISTS (SELECT 1 FROM Sede WHERE Id = p_IdSede) THEN
-        -- Verificar que exista el servicio
-        IF EXISTS (SELECT 1 FROM Servicio WHERE Id = p_IdServicio) THEN
-            -- Verificar que no exista ya la relación
-            IF NOT EXISTS (
-                SELECT 1
-                FROM SedeServicio
-                WHERE IdSede = p_IdSede
-                  AND IdServicio = p_IdServicio
-            ) THEN
-                INSERT INTO SedeServicio (
-                    IdSede, IdServicio, Descripcion, Capacidad, Precio, HoraInicio, HoraFinal
+    -- Verificar que no exista otro usuario con los mismos datos.
+    IF NOT EXISTS (SELECT 1 FROM Usuario WHERE Nombre = nombre AND Apellido=apellido AND IdTipoUsuario=idTipoUsuario) THEN
+        -- Verificar que el id en la tabla tipo de usuario exista 
+        IF EXISTS (SELECT 1 FROM TipoUsuario WHERE Id = idTipoUsuario) THEN
+            -- Verificar que no haya otro usuario con el mismo email en la tabla UsuarioSede.
+            IF NOT EXISTS (SELECT 1 FROM UsuarioSede WHERE Email=email) THEN
+                INSERT INTO USUARIO (
+                    Email, Telefono, Nombre, Apellido, FotoUrl, FechaNacimiento, IdTipoUsuario
                 )
                 VALUES (
-                    p_IdSede, p_IdServicio, p_Descripcion, p_Capacidad, p_Precio, p_HoraInicio, p_HoraFinal
+                    email, telefono, nombre, apellido, fotourl, fechaNacimiento, idTipoUsuario
                 );
             END IF;
         END IF;
