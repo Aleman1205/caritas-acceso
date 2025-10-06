43885d9b65bcfe6efb6960199915880a4c59ea17
Omar Caballero <mromarsin@gmail.com>
2025-10-05 23:38:11 -0400

    Made Loading Screen, Terms Screen and Home Screen Type shit Twin i really be representing this shit type shit

---
+++ frontend-app/app/src/main/java/com/example/caritasapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.caritasapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val CaritasBlueTeal = Color(0xFF009CA6)
+val CaritasNavy = Color(0xFF003B5C)
+val CaritasLight = Color(0xFFD1E0D7)
+val CaritasGray = Color(0xFF888B8D)
+val CaritasPurple = Color(0xFFA15A95)
+val CaritasOrange = Color(0xFFFF7F32)
