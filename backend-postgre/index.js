e23cda2349e46d18256c9d0a074dd6b02acd522f
Omar Caballero <mromarsin@gmail.com>
2025-10-19 08:41:32 -0400

    updated conection

--- backend-postgre/index.js
+++ backend-postgre/index.js
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use("/movil", movilRouter);
 app.use("/web", webRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
