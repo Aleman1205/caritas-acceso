6e6c4c0ee2bf23111138fe00faf7b74b35687a5d
Angel Aleman <joseangelaleman62@gmail.com>
2025-10-21 23:34:12 -0600

    Restaurar backend al estado de 7eb4855 (antes de merge de feature/jose_backend)

--- backend-postgre/movil/routes/sedes.js
+++ backend-postgre/movil/routes/sedes.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { listarSedes, listarServiciosPorSede } from "../controllers/sedesController.js";
-
 export const router = express.Router();
 
-router.get("/", listarSedes);
-router.get("/:id/servicios", listarServiciosPorSede);
+router.get("/", (req, res) => {
+    res.send("Sedes route working");
+});
