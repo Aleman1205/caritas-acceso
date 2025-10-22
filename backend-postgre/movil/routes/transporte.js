26eebb88a0d5f116a581505c1eeeb386cc7d20b1
Omar Caballero <mromarsin@gmail.com>
2025-10-22 06:24:15 -0400

    fixed the transporte files, only had a get (no f'ing reason) so put a put and erased the "normalized" number

--- backend-postgre/movil/routes/transporte.js
+++ backend-postgre/movil/routes/transporte.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { obtenerDatosTransporte } from "../controllers/transporteController.js";
+import { solicitarTransporte } from "../controllers/transporteController.js";
 
 export const router = express.Router();
 
-router.get("/", obtenerDatosTransporte);
+router.post("/", solicitarTransporte);
